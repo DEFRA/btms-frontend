91c4f102b3655dcd42e7f776de0e0d62d1c8d3ff
Ian Shimmings <ishimmings@users.noreply.github.com>
2025-02-26 08:54:49 +0000

    Update start-localstack.sh to use mapped urls

--- compose/start-localstack.sh
+++ compose/start-localstack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ENDPOINT_URL=http://sqs.eu-west-2.localhost.localstack.cloud:4566
+ENDPOINT_URL=http://sqs.eu-west-2.localhost.localstack.cloud:4966
 
 export AWS_ENDPOINT_URL=$ENDPOINT_URL
 export AWS_REGION=eu-west-2
@@ -9,7 +9,7 @@ export AWS_ACCESS_KEY_ID=local
 export AWS_SECRET_ACCESS_KEY=local
 
 # S3 buckets
-# aws --endpoint-url=http://localhost:4566 s3 mb s3://my-bucket
+# aws --endpoint-url=$ENDPOINT_URL s3 mb s3://my-bucket
 
 # SNS/SQS topics, queues, subscriptions
 
